6120b6092614003cdec6a7f66d378b305ab2128b
HyphaSlayer <tiebo.croons@gmail.com>
2025-10-01 12:43:25 +0200

    Add Hotjar tracking script for enhanced user behavior analysis

--- detail.php
+++ detail.php
@@ -171,6 +171,17 @@ $brandSubtitle = 'Digital Designer'; // You can add this to settings table if ne
 
   gtag('config', 'G-Y6MVVR1W8Q');
 </script>
+<script>
+    (function (c, s, q, u, a, r, e) {
+        c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+        c._hjSettings = { hjid: a };
+        r = s.getElementsByTagName('head')[0];
+        e = s.createElement('script');
+        e.async = true;
+        e.src = q + c._hjSettings.hjid + u;
+        r.appendChild(e);
+    })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 6534877);
+</script>
     <link rel="icon" type="image" href="img/testi-1.png" />
 
     <!-- External CSS -->
